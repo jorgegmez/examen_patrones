a642716115ab516548cc8691c65044cc74188f9b
Jorge Gomez <jgomezm@ucenfotec.ac.cr>
2018-02-21 19:28:19 -0600

    Prueba: String "V"
    Falla porque no cumple con los requerimeintos

--- src/test/java/com/ucenfotec/patrones/logic/ProcesadorRomanoTest.java
+++ src/test/java/com/ucenfotec/patrones/logic/ProcesadorRomanoTest.java
@@ -30,5 +30,11 @@ public class ProcesadorRomanoTest {
 		assertEquals(3, numRomano.Convierte("III"));
 	}
 	
+	@Test
+	public void StringV() {
+		ProcesadorRomano numRomano = new ProcesadorRomano();
+		assertEquals(5, numRomano.Convierte("v"));
+	}
+	
 
 }
