6f30c5c5f3da1b0d41dc37d0fb04168cca6acb16
Jorge Gomez <jgomezm@ucenfotec.ac.cr>
2018-02-21 19:11:03 -0600

    Refactoring: String Vacio
    Superada con una mejor solucion

--- src/main/java/com/ucenfotec/patrones/logic/ProcesadorRomano.java
+++ src/main/java/com/ucenfotec/patrones/logic/ProcesadorRomano.java
@@ -2,9 +2,14 @@ package com.ucenfotec.patrones.logic;
 
 public class ProcesadorRomano {
 
-	public int Convierte(String string) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int Convierte(String numRomano) {
+		int valorARetornar = -1;
+		switch(numRomano) {
+			case "":
+				valorARetornar = 0;
+			break;
+		}
+		return valorARetornar;
 	}
 	
 
